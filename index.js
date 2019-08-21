eda5fad9fbcf308042666150fe4fbd43453ed51c
David Butland <dgbutland@gmail.comm>
2019-08-21 13:15:55 +0100

    refactored to be more generic and use common code

--- index.js
+++ index.js
@@ -13,170 +13,118 @@ AWS.config.update({
 
 const s3 = new AWS.S3({signatureVersion: 'v4', signatureCache: false, accessKeyId: config.keys.accessKeyId, secretAccessKey: config.keys.secretAccessKey});
 
-exports.resize_old = (event, context, callback) => {
+createImageThumbnail = ( bucket, folder, image, width ) => {
 
-	if ( ( !event.headers.Authorization ) || ( !event.headers.Authorization.startsWith("Bearer ") ) ) {
-		return response.invalid( { errorMessage: "Missing or Invalid Authorization Token" } );
-	}
+  return new Promise((resolve, reject) => {
+		const inputKey = 'private/'+folder+'/'+image ;
+		const outputKey = 'thumbnail/'+folder+'/'+image+'-'+width ;
 
-	const sub = validateJWT.getSub( event.headers.Authorization.slice(7) ) ;
+		var s3GetParams = {  Bucket: bucket, Key: inputKey } ;
 
-	if ( !sub ) {
-		callback( null, response.failure( { errorMessages: "Invalid Token"} ) ) ;
-		return false ;
-	}
-	
-	const body = JSON.parse( event.body ) ;
-	const table = 'sans-images' ;
-	var image = {} ;
-	image.userId    = sub ; 
-	image.imageId   = body.imageId
-	image.folderId  = body.folderId ;
-	image.name      = body.name ;
-	image.type      = body.type ;
-	image.size      = body.size ;
-	image.thumbnail = body.thumbnail ;
-	image.createdAt = body.createdAt ;
-
-	image.thumbnail = image.imageId + '-300' ;
-
-	var bucket   = "private.sans-website.com"
-	var key      = "private/" + image.folderId + "/" + image.imageId ;
-	var thumbkey = "private/" + image.folderId + "/" + image.thumbnail ;
-
-	console.log( JSON.stringify( image ) ) ;
-
-	var s3GetParams = {  Bucket: bucket, Key: key } ;
-
-	console.log( JSON.stringify( s3GetParams ) ) ;
-
-	s3.getObject( s3GetParams, function(err, data) {
-		if (err) { 
-			console.log("Failed to get image for resizing") ;
-			console.log(err, err.stack); 
-		} 
-		else {
-
-			const { Body, ContentType } = data
-			const imageData = new Buffer.from(Body)
-			const tasks = { width: 300 } ;
-
-			sharp(imageData).resize(tasks).toBuffer().then(function(newFileInfo) {
-				
-				var s3PutParams = {  Bucket: bucket, Key: thumbkey, ContentType, Body: newFileInfo } ;
-
-				s3.putObject( s3PutParams, function( err, data) {
-					if (err) { 
-						console.log("Failed to save resized image") ;
-						console.log(err, err.stack); 
-						callback( null, response.failure({ status: false }) );
-					} 
-					else {
-						persist.put( table, image ).then( function( data ) {
-							callback( null, response.success( image))
-						}).catch ( function (error ) {
-							console.log("Failed to update image record") ;
-							console.log( "error=" + error) ;
-							callback( null, response.failure({ status: false }) );
-						}) ;
-					}
-				}) ;
-			})
-			.catch(function(err) {
-				console.log("Resizing failed") ;
-				console.log("Error occured", err);
-				callback( null, response.failure({ status: false }) );
-			});
-		}
-	}) ;
-}
-
-exports.resize = (event, context, callback) => {
-
-	if ( ( !event.headers.Authorization ) || ( !event.headers.Authorization.startsWith("Bearer ") ) ) {
-		return response.invalid( { errorMessage: "Missing or Invalid Authorization Token" } );
-	}
-
-	const sub = validateJWT.getSub( event.headers.Authorization.slice(7) ) ;
-
-	if ( !sub ) {
-		callback( null, response.failure( { errorMessages: "Invalid Token"} ) ) ;
-		return false ;
-	}
-	
-	const table = 'sans-images' ;
-	const imageId = event.pathParameters.imageid ;
-  const key = { userId: sub, imageId: imageId } 
-
-	persist.get( table, key ).then( function( record ) {
-
-		var image = record.Item ;
-		image.thumbnail = image.imageId + '-300' ;
-
-		var bucket   = "private.sans-website.com"
-		var key      = "private/" + image.folderId + "/" + image.imageId ;
-		var thumbkey = "private/" + image.folderId + "/" + image.thumbnail ;
-	
-		console.log( JSON.stringify( image ) ) ;
-	
-		var s3GetParams = {  Bucket: bucket, Key: key } ;
-	
-		console.log( JSON.stringify( s3GetParams ) ) ;
-	
 		s3.getObject( s3GetParams, function(err, data) {
 			if (err) { 
 				console.log( "Error reading image - ", err, err.stack );
-				callback( null, response.failure({ status: false }) );
-			} 
-			else {
+				reject( 'Error reading image' ) ;
+			} else {
 				const { Body, ContentType } = data
 				const imageData = new Buffer.from(Body)
-				const tasks = { width: 300 } ;
-	
+				const tasks = { width: width } ;
+
 				sharp(imageData).resize(tasks).toBuffer().then(function(newFileInfo) {
-					var s3PutParams = {  Bucket: bucket, Key: thumbkey, ContentType, Body: newFileInfo } ;
-	
+					var s3PutParams = {  Bucket: bucket, Key: outputKey, ContentType, Body: newFileInfo } ;
+
 					s3.putObject( s3PutParams, function( err, data) {
 						if (err) { 
 							console.log( "Error writing image - ", err, err.stack );
-							callback( null, response.failure({ status: false }) );
+							reject( 'Error writing image' ) ;
+						} else {
+							console.log( "Resized image " + image, " width " + width ) ;
+							resolve( 'Resized image ' + image + " to width " + width ) ;
 						} 
-						else {
-							persist.put( table, image ).then( function( data ) {
-								callback( null, response.success( image ) )
-							}).catch ( function( err ) {
-								console.log( "Error updating table image - ", err, err.stack );
-								callback( null, response.failure({ status: false }) );
-							}) ;
-						}
 					}) ;
 				})
 				.catch(function(err) {
 					console.log( "Error resizing image - ", err, err.stack );
-					callback( null, response.failure({ status: false }) );
+					reject( 'Error resizing image' ) ;
 				});
 			}
-		}) ;	
-	})
-	.catch(function(err) {
-		console.log( "Error getting image record - ", err, err.stack );
-		callback( null, response.failure({ status: false }) );
+		}) ;
 	});
 }
 
-exports.resizeTest = (event) => {
+createImageThumbnails = async(bucket, folder, image ) => {
+	const sizes  = [300,600,900,1200,1500,1800] ;
+	
+	var resizing = [] ;
+ 
+	for ( i = 0 ; i < sizes.length ; i++ ) {
+		console.log( i, sizes[i] ) ;
+		resizing.push( createImageThumbnail( bucket, folder, image, sizes[i] ) ) ;
+	}
 
-	console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
-	var bucket   = event.Records[0].s3.bucket.name;
-	var key      = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));  
-	var thumbkey = key + '-300' ;
+	await Promise.all(resizing);
 
-	path = key.split( "/" ) ;
-
-	console.log( path ) ;
-	console.log( bucket, key ) ;
-
+	console.log("Finished resizing" ) ;
 }
+
+createFolderThumbnails = async( user, folder ) => {
+
+	const quyen = '832bb986-871d-4bd2-a832-9e7134265604' ; // temp for now
+
+	const bucket   = "private.sans-website.com"
+	const table = 'sans-images' ;
+	const index = 'userid-folderid-index' ;
+	const expression = "userId = :u and folderId = :f" ;
+	const values = {":u": quyen, ":f": folder } ;
+
+	persist.readIndex( table, index, expression, values ).then( function ( data ) { 
+		
+		var images = data.Items ;
+		images.map ( image => {
+			await createImageThumbnails( bucket, folder, image ) ;
+		}) ;
+		
+	}).catch ( function( err ) {
+		console.log( "Error reading images - ", err, err.stack );
+	}) ;
+}
+
+exports.createImageThumbnailsTrigger = async (event) => {
+
+	console.log("createImageThumbnailsTrigger entry ", util.inspect(event, {depth: 5}));
+
+	const bucket = event.Records[0].s3.bucket.name;
+	const key    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+	const path   = key.split( "/" ) ;
+	const folder = path[1] ;
+	const image  = path[2] ;	
+
+	await createThumbnails( bucket, folder, image ) ;
+
+	console.log( "createImageThumbnailsTrigger exit" ) ;
+}
+
+exports.createFolderThumbnailsTrigger = async (event, context, callback) => {
+	
+	console.log("createFolderThumbnailsTrigger entry ", util.inspect(event, {depth: 5}));
+
+	if ( ( !event.headers.Authorization ) || ( !event.headers.Authorization.startsWith("Bearer ") ) ) {
+		return response.invalid( { errorMessage: "Missing or Invalid Authorization Token" } );
+	}
+
+	const sub = validateJWT.getSub( event.headers.Authorization.slice(7) ) ;
+
+	if ( !sub ) {
+		callback( null, response.failure( { errorMessages: "Invalid Token"} ) ) ;
+		return false ;
+	}
+	
+	const folderId = event.pathParameters.folderid ;
+
+	await this.createFolderThumbnails( sub, folderId ) ;
+
+	console.log( "createFolderThumbnailsTrigger exit" ) ;
+}}
 
 exports.resizeImage = (event) => {
 
